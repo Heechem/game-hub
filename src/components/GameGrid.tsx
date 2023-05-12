8eb3534da73a1a58b1c147ca04769b0659f2de08
Heechem <hichempeter@gmail.com>
2023-05-11 20:13:54 -0600

    clean up the game cards

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -22,7 +22,7 @@ const GameGrid = ({ gameQeury }: Props) => {
   return (
     <>
       {error && <Text>{error}</Text>}
-      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} p='12px' spacing={3}>
+      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} p='12px' spacing={6}>
         {isLoading &&
           skeletons.map((skeleton) => (
             <GameCardContainer key={skeleton}>
