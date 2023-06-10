f1905c79add75995efa5bb3df498dace9c166d1b
Heechem <hichempeter@gmail.com>
2023-06-10 08:20:56 -0600

    Fix the navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,6 +2,7 @@ import { HStack, Image, Text } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
@@ -9,10 +10,13 @@ const NavBar = () => {
       justifyContent={"space-around"}
       p="10px"
     >
-      <Image
-        src={logo}
-        boxSize="60px"
-      />
+      <Link to="/">
+        <Image
+          src={logo}
+          boxSize="60px"
+          objectFit={"cover"}
+        />
+      </Link>
       <SearchInput />
       <ColorModeSwitch />
     </HStack>
--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,17 +1,11 @@
-import React from "react";
+import { GridItem, Heading, SimpleGrid, Spinner } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
 import useGame from "../hooks/useGame";
-import { GridItem, Heading, SimpleGrid, Spinner, Text } from "@chakra-ui/react";
-import GameCardSkeloton from "../components/GameCardSkeloton";
 
-import { SiExercism } from "react-icons/si";
 import ExpandableText from "../components/ExpandableText";
-import DefinitionItem from "../components/DefinitionItem";
-import platforms from "../Data/platforms";
-import CriticScore from "../components/CriticScore";
 import GameAttributes from "../components/GameAttributes";
-import GameTrailer from "../components/GameTrailer";
 import GameScreenshots from "../components/GameScreenshots";
+import GameTrailer from "../components/GameTrailer";
 
 const GameDetailPage = () => {
   const { slug } = useParams();
